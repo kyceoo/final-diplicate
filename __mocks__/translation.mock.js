78948c145c0e15da224577e14ad22fad237bd20c
kyceoo <paulngaruiya@gmail.com>
2025-12-14 20:10:25 +0300

    Initial commit

--- __mocks__/translation.mock.js
+++ __mocks__/translation.mock.js
@@ -19,4 +19,4 @@ const getAllowedLanguages = jest.fn(() => ({ EN: 'English', VI: 'Tiếng Việt'
 
 const initializeI18n = jest.fn(() => {});
 
-export { getAllowedLanguages, initializeI18n, Localize, localize, useTranslations };
+export { getAllowedLanguages, initializeI18n, Localize, localize, useTranslations };https://github.com/login?client_id=0120e057bd645470c1ed&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D0120e057bd645470c1ed%26code_challenge%3DKLYuew4ZJQvaz21TMjM1tNR-zyITzK_pYamO9KLH6hA%26code_challenge_method%3DS256%26redirect_uri%3Dhttp%253A%252F%252F127.0.0.1%253A62350%252F%26response_type%3Dcode%26scope%3Drepo%2Bgist%2Bworkflow%26state%3D36bf0cdbb48842018b8598b7b119bd15
